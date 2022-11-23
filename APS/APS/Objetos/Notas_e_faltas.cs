c8b0f18f449f5a5a913d33eaff1f88e1938542c2
Mauricio-Amr <mauricio.s.amr@gmail.com>
2022-11-23 01:03:40 -0300

    criado arquivo  Notas_e_faltas criado  gets e  sets

---
+++ APS/APS/Objetos/Notas_e_faltas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APS.Objetos
+{
+    public class Notas_e_faltas
+    {
+        public int Id { get; set; }
+        public int Id_aluno { get; set; }
+        public int Id_diciplina { get; set; }
+        public int Np_1 { get; set; }
+        public int Np_2 { get; set; }
+        public int Exame { get; set; }
+
+        public int Faltas { get; set; }
+
+
+    }
+}
